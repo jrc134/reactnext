4934580bd50f6aa96ccfedb27f850d2c4bcfa7e2
jrc134 <jrc134@humboldt.edu>
2022-01-22 13:11:36 -0800

    add sass

---
+++ pages/device.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function Device() {
+    return (
+        <div className={styles.title}>
+        <p>Device</p>
+        </div>
+        
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Device from './device'
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello, Welcome to my website!</h1>
+      <Device />
     </div>
   )
 }
